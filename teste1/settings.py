612d68c0cae1fbe8efd1c24083b91f2c91f280fb
caio henrique <caiohsv07@gmail.com>
2023-11-14 12:20:35 -0300

    1

--- teste1/settings.py
+++ teste1/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     "mathfilters",
-    'whitenoise.runserver_nostatic',    
+    'whitenoise.runserver_nostatic', 
 ]
 
 MIDDLEWARE = [
