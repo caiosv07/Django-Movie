53874ba45c03eaafdce16cb63bf92f3eed415806
caio henrique <caiohsv07@gmail.com>
2023-11-14 11:28:58 -0300

    n

--- teste1/settings.py
+++ teste1/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'teste2',
+    'crispy_forms',
+    'crispy_bootstrap5',
+    "mathfilters",
 ]
 
 MIDDLEWARE = [
